0cff6d30a27c07235e515b9ea87d994c8dd906d5
JimmyYu01 <126685529+JimmyYu01@users.noreply.github.com>
2026-09-22 20:56:16 +0800

    v1.0.1更新

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import astrbot.api.message_components as Comp
+from astrbot.api import AstrBotConfig
 from astrbot.api.event import AstrMessageEvent, filter
 from astrbot.api.star import Context, Star, register
 
 
 @register(
-    "astrbot_plugin_pokeable", "JimmyYu01", "让AstrBot能够主动处理戳一戳消息段。", "1.0.1"
+    "astrbot_plugin_pokeable",
+    "JimmyYu01",
+    "让AstrBot能够主动处理戳一戳消息段。",
+    "1.0.1",
 )
 class MyPlugin(Star):
-    def __init__(self, context: Context):
+    def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
+        self.config = config
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
     @filter.llm_tool()
     async def send_poke(self, event: AstrMessageEvent, user_id: str = ""):
-        """戳一戳指定的用户。
+        """戳一戳指定用户。
 
         Args:
-            user_id(string): 指定的用户的QQ号。可以从当前消息中获取，如果留空则指定为当前消息发送者。
+            user_id(string): 指定用户的QQ号。如果留空则指定为当前消息发送者。
         """
         if event.get_platform_name() == "aiocqhttp":
             from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import (
@@ -27,7 +32,6 @@ class MyPlugin(Star):
 
             assert isinstance(event, AiocqhttpMessageEvent)
             client = event.bot
-
             user_poked = str(user_id).strip() or event.get_sender_id()
             if event.is_private_chat():
                 await client.call_action("send_poke", user_id=user_poked)
@@ -40,9 +44,11 @@ class MyPlugin(Star):
             return f"戳了一下{user_poked}"
         return "当前消息平台不支持戳一戳"
 
-    @filter.platform_adapter_type(filter.PlatformAdapterType.AIOCQHTTP)
-    async def repoke(self, event: AstrMessageEvent):
-        """回复别人的戳一戳。"""
+    @filter.event_message_type(filter.EventMessageType.ALL)
+    async def reply_poke(self, event: AstrMessageEvent):
+        """主动回复戳一戳。"""
+        if not self.config.get("reply", False):
+            return
         for message in event.get_messages():
             if (
                 isinstance(message, Comp.Poke)
